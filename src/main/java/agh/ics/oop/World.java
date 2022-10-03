8a213c6e1a4200bb58a4de1e3914bf84a27fc9f9
Patryk Klatka <patryk.klatka@gmail.com>
2022-10-03 23:14:32 +0200

    Lab 1 tasks 14-15

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -5,13 +5,24 @@ import java.lang.System;
 public class World {
 
     public static void run(String[] args){
-        if(args.length == 0) return;
-
-        for (int i = 0; i < args.length-1; i++) {
-            System.out.print(args[i] + ", ");
+        System.out.println("Start");
+        for(String arg : args){
+            switch (arg){
+                case "f":
+                    System.out.println("Zwierzak idzie do przodu");
+                    break;
+                case "b":
+                    System.out.println("Zwierzak idzie do tyłu");
+                    break;
+                case "r":
+                    System.out.println("Zwierzak skręca w prawo");
+                    break;
+                case "l":
+                    System.out.println("Zwierzak skręca w lewo");
+                    break;
+            }
         }
-        System.out.println(args[args.length-1]);
-
+        System.out.println("Stop");
 }
 
     public static void main(String[] args){
