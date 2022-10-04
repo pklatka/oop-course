67b2cd2e8e32e7fbf09110b63c1dfe95e4de2df6
Patryk Klatka <patryk.klatka@gmail.com>
2022-10-04 21:56:15 +0200

    Lab 1 task 26

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,44 +1,39 @@
 package agh.ics.oop;
 
-import java.lang.System;
+import java.util.Arrays;
+import java.util.stream.*;
 
 public class World {
 
     public static void run(Direction[] args){
         System.out.println("Start");
-        for(Direction arg : args){
-            switch (arg){
-                case FORWARD:
-                    System.out.println("Zwierzak idzie do przodu");
-                    break;
-                case BACKWARD:
-                    System.out.println("Zwierzak idzie do tyłu");
-                    break;
-                case RIGHT:
-                    System.out.println("Zwierzak skręca w prawo");
-                    break;
-                case LEFT:
-                    System.out.println("Zwierzak skręca w lewo");
-                    break;
+        Stream.of(args).map(e->
+            switch (e) {
+                case FORWARD -> "Zwierzak idzie do przodu\n";
+                case BACKWARD ->"Zwierzak idzie do tyłu\n";
+                case RIGHT -> "Zwierzak skręca w prawo\n";
+                case LEFT -> "Zwierzak skręca w lewo\n";
+                default -> "";
             }
-        }
+        ).forEach(System.out::print);
         System.out.println("Stop");
 }
 
     public static void main(String[] args){
         System.out.println("system wystartował");
 
-        // Convert string to enum
-        Direction enumArgs[] = new Direction[args.length];
-        for (int i = 0; i < args.length; i++) {
-            enumArgs[i] = switch (args[i]){
+        Direction[] enumArgs = Arrays.stream(args).map(e->{
+            // Swap string to enum
+            // There might be a better solution using Direction.valueOf(String e)
+            // but it requires enum names to be the same as string
+            return switch (e){
                 case "f" -> Direction.FORWARD;
                 case "b" -> Direction.BACKWARD;
                 case "r" -> Direction.RIGHT;
                 case "l" -> Direction.LEFT;
                 default -> Direction.UNDEFINED;
             };
-        }
+        }).toArray(Direction[]::new);
 
         run(enumArgs);
         System.out.println("system zakończył działanie");
